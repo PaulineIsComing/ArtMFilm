eee5123a30c78cd47268fbcc0a3a2571fdbe82a2
PierreMarchetti <pierre.marh@gmail.com>
2019-01-15 13:41:14 +0100

    recherche artiste

--- src/pages/artiste/artiste.ts
+++ src/pages/artiste/artiste.ts
@@ -13,10 +13,12 @@ import { ArtisteDetailsPage } from '../artiste-details/artiste-details';
 export class ArtistePage {
 
   tableau:Artiste[];
-  img_path:String = 'https://image.tmdb.org/t/p/original';
+  img_path: string = 'https://image.tmdb.org/t/p/original';
+  text: string;
+  basic_url: string = 'https://api.themoviedb.org/3/person/popular?api_key=a11c8337b79630d0217518e8f78dc5ad&language=en-US&page=1'
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private httpClient:HttpClient) {
-    this.httpClient.get<Artiste[]>('https://api.themoviedb.org/3/person/popular?api_key=a11c8337b79630d0217518e8f78dc5ad&language=en-US&page=1').subscribe(
+    this.httpClient.get<Artiste[]>(this.basic_url).subscribe(
       (result:Artiste[])=>{
         this.tableau = result;
       },
@@ -29,6 +31,25 @@ export class ArtistePage {
   //affiche les détails d'un artiste dans une nouvelle page
   showDetails(id: number) {
     this.navCtrl.push(ArtisteDetailsPage, {id: id})
+  }
+
+  //effectue une de film requête par rapport aux champs du formulaire et affiche les résultats
+  search(){
+    var url;
+    if(this.text == null || this.text.length == 0){
+      url = this.basic_url;
+    }
+    else {
+      url = `https://api.themoviedb.org/3/search/person?api_key=a11c8337b79630d0217518e8f78dc5ad&language=fr&query=${this.text}`;
+    }
+    this.httpClient.get<Artiste[]>(url).subscribe(
+      (result:Artiste[])=>{
+        this.tableau = result;
+      },
+      (error)=>{
+        console.log(error);
+      }
+    )
   }
 
   ionViewDidLoad() {
